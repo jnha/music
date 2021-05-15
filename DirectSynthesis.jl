2d04237fb11a9e4f00864a36746459290d7bab6a
jnha <32179923+jnha@users.noreply.github.com>
2021-05-15 14:38:47 -0400

    Moving Tone to DirectSynthesis

--- DirectSynthesis.jl
+++ DirectSynthesis.jl
@@ -27,7 +27,7 @@ function plotfreqs(tone, title)
 end
 
 # ╔═╡ 4bd75a55-4457-4625-a184-a2f7490cbf0e
-md"## Sinusoid wave
+md"## Sinusoids
 The builtin sin() and cos() functions produce sinusoidal waves, they are roughly
 equivalent for this, being phase shifted versions of each other."
 
@@ -41,9 +41,15 @@ sine_tone = sin.(freqs)
 plotfreqs(sine_tone, "Sine Spectrum")
 
 # ╔═╡ dd813478-66a1-4d3e-9872-322e7b521e11
-md"## Modulo waveforms: Sawtooth
-Using the periodic behavior modular division we get a sawtooth wave.
-However, making a sawtooth directly like this causes aliasing due to the discontinuity in the waveform. The aliasing shows up as noise in the signal that is visible on the frequency graph."
+md"## Modulo waveforms
+### Sawtooth
+Using the periodic behavior of the remainder operator we get a sawtooth wave.
+However, making a sawtooth directly like this gives aliasing
+since this equivalent to sampling an analog sawtooth wave with all harmonics.
+Harmonics higher than half the sampling rate cannot accurately be represented
+and appear as lower frequencies when reproducing the sound.
+This aliasing shows up as noise in the signal that is visible on the frequency graph.
+"
 
 # ╔═╡ a2357e48-116a-4df1-a52d-100533bad25d
 function saw(t)
@@ -60,8 +66,13 @@ saw_tone = saw.(freqs)
 plotfreqs(saw_tone, "Aliased Saw Spectrum")
 
 # ╔═╡ dfa8eb03-2955-4bb6-b9ca-eb7cd6583e57
-md"### Modulo waveforms: Triangle
-If add a ramp down to the sawtooth wave we remove the discontinuity and get a triangle wave. Most of the aliasing is gone but there is still some from the sharp cusp."
+md"
+### Triangle
+The jagged shape of the sawtooth wave, with it's sharp discontinuity,
+is why it's higher harmonics are so loud.
+If we use two ramps to remove the discontinuity we get a triangle wave 
+with harmonics that fall off more quickly.
+The aliasing is reduced since the harmonics are softer by the time they reach the frequency limit. It also no longer has even harmonics, since the second half is the first half upside down."
 
 # ╔═╡ a9f4a512-c8b4-474a-a93c-2cef99db534c
 function tri(t)
@@ -79,53 +90,47 @@ plotfreqs(tri_tone, "Triangle Spectrum")
 
 # ╔═╡ 99708158-d74e-4311-a10f-d0ac4f79d4f2
 md" ### Polynomial waveforms
-We can use polynomials to get continuous waveforms of various shapes by
-wrapping the input modulo 2 pi and ensuring that the two endpoints are equal.
-We can get a smooth waveform by setting the derivitives of the endpoints equal.
+We can use polynomials of a sawtooth wave to get various periodic waveforms.
+If the two endpoints are equal the wave will be continuous.
+If the derivitives at the endpoints are equal the waveform will be smooth.
+Smooth waveforms have harmonics that fall off even faster than continuous waveforms.
+
+Using a parabola we get harmonic roll-off similar to a triangle wave
+but including the even harmonics:
 "
 
 # ╔═╡ 1f127661-47b0-4498-9fc2-857f314b6869
 function poly2(t)
-	""" 
-	We set the first and last coefficients to 1 and 0 respectively as our result
-	will be normalized to the range [-1,1] anyway
-	f = x^2 + bx
-	1 + b = f(-1) = f(-1) = 1-b
-	2b = 0, b = 0
-	f = x^2
-	then we scale and shift into [-1, 1]
-	2(t*t) - 1
+	"""
+	Parabolic waveform
+	since t^2 gives output in [0, 1] we use 2t^2-1 to get output in [-1,1]
+	
 	Note that since f' = 2x + b is linear we can't make a smooth function
+	since no two points can be equal
 	"""
 	t = t/pi %2 -1
 	2*t*t - 1
 end
 
-# ╔═╡ 8802e5fe-8416-4c67-a1f0-b99bb6af3e3f
-plot(t, poly2.(t))
-
 # ╔═╡ 99f21cb1-b50f-4e8b-9d97-8bf5297afbec
 poly2_tone = poly2.(freqs)
 
+# ╔═╡ 8802e5fe-8416-4c67-a1f0-b99bb6af3e3f
+plot(t, poly2.(t))
+
 # ╔═╡ b561d76e-e2a7-4c3d-9677-42f8b5661b7c
-# poly2 still has >-40db aliasing
-# It's kind of like a triangle wave but with even harmonics
+# Kind of like a triangle wave but with even harmonics
 plotfreqs(poly2_tone, "Poly2 Spectrum")
 
-# ╔═╡ e060b886-4af2-4692-8882-79ceabe64ff1
-function normalize(tone)
-	""" Normalizing polynomial waveforms directly is annoying
-	so we will just use this function"""
-	m = minimum(tone)
-	M = maximum(tone)
-	2*(tone.-m)/(M-m) .-1
-end
+# ╔═╡ 09d7e4f3-fed8-4fc8-b598-03ecc8e75d6f
+md"With a cubic we can get a smooth function.
+This has harmonics that fall off quickly enough that there is no noticable aliasing,
+even at this low of a samplerate.
+"
 
 # ╔═╡ aff77549-c6da-4329-8e92-5f1fb561ece6
 function poly3(t)
 	"""
-	With a cubic we can get a smooth function
-
 	f(x) = x^3 + bx^2 + cx
 	function is continuous when
 	1 + b + c = f(1) = f(-1) = -1 + b - c
@@ -137,24 +142,36 @@ function poly3(t)
 	4b = 0, b = 0
 
 	f(x) = x^3 - x
-	f'(x) = 3x^2-1 = 0  when x = +-sqrt(1/3)
-	f(sqrt(3)/3) = sqrt(3)/9-sqrt(3)/3 = -2sqrt(3)/9 
-	
+	f'(x) = 3x^2-1 = 0  when x = +-1/sqrt(3)
+	f(-1/sqrt(3)) = -1/sqrt(3)/3+1/sqrt(3) = 2/(3sqrt(3))
+	We scale by the inverse 3sqrt(3)/2
 	"""
-	b = 0
 	t = t/pi%2-1
-	x = 3*sqrt(3)/2*t*(t*(t+b) - 1)
+	x = 3*sqrt(3)/2*t*(t*t - 1)
 end
+
+# ╔═╡ e91b624c-cef9-4f74-8bf0-76a484c9c895
+poly3_tone = poly3.(freqs)
 
 # ╔═╡ 6880cf94-23ee-4a96-8b9f-0417fc239374
 plot(t, poly3.(t))
 
-# ╔═╡ e91b624c-cef9-4f74-8bf0-76a484c9c895
-poly3_tone = normalize(poly3.(freqs))
-
 # ╔═╡ 4caa26e8-c5ce-4d30-b80b-d3ed7659efa0
-# Here there is no >-40db aliasing, so the aliasing is now negligible
+# Here there is no aliasing >-40db
 plotfreqs(poly3_tone, "Poly3 Spectrum")
+
+# ╔═╡ 1701a11b-b17b-499a-bf5f-0d721c026590
+md"As we smooth the function more by setting higher derivitives equal
+the polynomials approach a pure sine tone"
+
+# ╔═╡ e060b886-4af2-4692-8882-79ceabe64ff1
+function normalize(tone)
+	""" Normalizing polynomial waveforms directly is annoying
+	so we will use this function instead of calculating minimum and maximums"""
+	m = minimum(tone)
+	M = maximum(tone)
+	2*(tone.-m)/(M-m) .-1
+end
 
 # ╔═╡ 85236c40-9c9c-4e00-b8e8-54787a5760af
 function poly4(t, b)
@@ -170,11 +187,11 @@ function poly4(t, b)
 	t = (((t + b)*t -2)*t - b)*t
 end
 
-# ╔═╡ 788ec89e-c1d0-498b-8e8c-82de0bc4eda6
-plot(t, normalize(poly4.(t, 0)))
-
 # ╔═╡ f1a60f5c-53c6-4533-b3ed-e715fb67d7ba
 poly4_tone = normalize(poly4.(freqs, 0))
+
+# ╔═╡ 788ec89e-c1d0-498b-8e8c-82de0bc4eda6
+plot(t, normalize(poly4.(t, 0)))
 
 # ╔═╡ e24388fa-d474-4770-95a0-d34627ae7a04
 # No overtones reach the niquist rate, no aliasing at all
@@ -311,64 +328,6 @@ plotfreqs(quart_tone, "Quartic Spectrum")
 # ╔═╡ d5e54701-36a3-45f9-ac2c-8917b2e3a09d
 md"Polynomial methods are cheap to compute and have more tone than a pure sine wave but aren't inherently bandlimited and so can't make brighter sounds similar to analog square or saw waves without aliasing."
 
-# ╔═╡ 70df2c12-dff7-49c5-b489-f8c20be01c85
-md"## Combining sine waves
-Since our only waveform that is bandlimited by default is sin (and cos)
-we can make other waveforms with little to know aliasing by combining multiple
-sin waves in various ways."
-
-# ╔═╡ 0c0816cd-8555-4283-bce4-0c00773cbbff
-md"### Discrete Summation fomulas
-Discrete summation fomulas can be used to create waveforms with
-harmonics at regular intervals.
-Discrete summation formulae can be parameterized and combined to make a variety of spectra but here we will look at basic every harmonic (saw-like) and an odd harmonic (square-like) tones"
-
-
-# ╔═╡ 68554d76-6906-4271-9dc4-a4e405a3214a
-# A naive sin sum
-function sine_sum(a, t, b, n)
-	sum(a^i*sin(t + i*b) for i=0:n)
-end
-
-# ╔═╡ c2d1d0e0-7bac-4287-8487-022691dd8c42
-# An equivalent closed form calculation
-function dsf(a, t, b, n)
-	"""
-	a: ratio between adjacent harmonics
-	t: base frequency
-	b: frequency offset between harmonics
-	   (harmonics of t + mb for 0 <= m <= n)
-	n: number of harmonics
-	"""
-	out = sin(t) - a*sin(t-b)
-	out -= a^(n+1)*(sin(t + (n+1)*b) - a*sin(t + n*b))
-	out /= 1 + a*a - 2*a*cos(b)
-end
-
-# ╔═╡ e773e9b8-2e0c-4e99-9c5a-494a5cfb1c94
-begin
-	# Demonstration that they are the same, try random values
-	a, t2, b, n = .234, .34534, .234, 34
-	sine_sum(a, t2, b, n), dsf(a, t2, b, n)
-end
-
-# ╔═╡ 1152ff30-eb29-4550-8503-44df407838eb
-function dsfsaw(t)
-	dsf(.8, t, t, 16)/3
-end
-
-# ╔═╡ cb45d234-0e1d-4ce6-9dce-86320d269be2
-plot(t, dsfsaw.(t))
-
-# ╔═╡ 5dd7f92f-bcf1-4373-91f9-de25487c0e95
-dsfsaw_tone = normalize(dsfsaw.(freqs))
-
-# ╔═╡ e2520f9b-66e0-488f-8a47-ddefdd161d62
-plotfreqs(dsfsaw_tone, "DSF saw Spectrum")
-
-# ╔═╡ 7087e2d5-e66e-4d19-9478-6bf13a8cfed6
-md"DSF can be used to create very bright sounds like a saw or square without aliasing. It also can mimic the sharp cutoff above a limit that a filter would produce. However the amplitude of the harmonics is geometric $(a^n)$ rather than harmonic $(1/n)$ so an exact bandlimited saw or square can't be replicated"
-
 # ╔═╡ Cell order:
 # ╟─f8ba933c-a1e3-11eb-2263-17f14870c1fa
 # ╠═06240e71-df45-4f63-b797-4042432e6716
@@ -378,29 +337,31 @@ md"DSF can be used to create very bright sounds like a saw or square without ali
 # ╠═664b9502-13de-4fdf-971c-66cf0ea47c3b
 # ╠═5e805acc-c6b1-4de5-8bc1-5bdcbd84ca04
 # ╠═2a51d397-322b-414b-9d9f-a1e8ba6b2364
-# ╟─dd813478-66a1-4d3e-9872-322e7b521e11
+# ╠═dd813478-66a1-4d3e-9872-322e7b521e11
 # ╠═a2357e48-116a-4df1-a52d-100533bad25d
 # ╠═50824ffc-622d-4a00-acce-558cecf79e55
 # ╠═3ae066cb-b5f2-4913-8a73-20348a401026
 # ╠═7bd89cfd-9894-4644-b0e4-cae8a63cda9a
-# ╟─dfa8eb03-2955-4bb6-b9ca-eb7cd6583e57
+# ╠═dfa8eb03-2955-4bb6-b9ca-eb7cd6583e57
 # ╠═a9f4a512-c8b4-474a-a93c-2cef99db534c
 # ╠═f6f28913-cbfa-48f8-98ab-0d5ed267b350
 # ╠═231185ea-8913-490d-8fe8-261983552457
 # ╠═b627d7af-55d1-4d97-8f15-7f0d172f9b5a
 # ╟─99708158-d74e-4311-a10f-d0ac4f79d4f2
 # ╠═1f127661-47b0-4498-9fc2-857f314b6869
-# ╠═8802e5fe-8416-4c67-a1f0-b99bb6af3e3f
 # ╠═99f21cb1-b50f-4e8b-9d97-8bf5297afbec
+# ╠═8802e5fe-8416-4c67-a1f0-b99bb6af3e3f
 # ╠═b561d76e-e2a7-4c3d-9677-42f8b5661b7c
-# ╠═e060b886-4af2-4692-8882-79ceabe64ff1
+# ╠═09d7e4f3-fed8-4fc8-b598-03ecc8e75d6f
 # ╠═aff77549-c6da-4329-8e92-5f1fb561ece6
-# ╠═6880cf94-23ee-4a96-8b9f-0417fc239374
 # ╠═e91b624c-cef9-4f74-8bf0-76a484c9c895
+# ╠═6880cf94-23ee-4a96-8b9f-0417fc239374
 # ╠═4caa26e8-c5ce-4d30-b80b-d3ed7659efa0
+# ╠═1701a11b-b17b-499a-bf5f-0d721c026590
+# ╠═e060b886-4af2-4692-8882-79ceabe64ff1
 # ╠═85236c40-9c9c-4e00-b8e8-54787a5760af
-# ╠═788ec89e-c1d0-498b-8e8c-82de0bc4eda6
 # ╠═f1a60f5c-53c6-4533-b3ed-e715fb67d7ba
+# ╠═788ec89e-c1d0-498b-8e8c-82de0bc4eda6
 # ╠═e24388fa-d474-4770-95a0-d34627ae7a04
 # ╠═bf89825a-944b-4771-a619-e4c5d1220f61
 # ╠═076a215e-1335-4974-b61e-06fa4c9e9295
@@ -420,13 +381,3 @@ md"DSF can be used to create very bright sounds like a saw or square without ali
 # ╠═b649ead1-acf8-4390-bb9e-ae876a1c7f48
 # ╠═c492d40f-fc0d-4b06-a925-7c75bff32503
 # ╟─d5e54701-36a3-45f9-ac2c-8917b2e3a09d
-# ╠═70df2c12-dff7-49c5-b489-f8c20be01c85
-# ╠═0c0816cd-8555-4283-bce4-0c00773cbbff
-# ╠═68554d76-6906-4271-9dc4-a4e405a3214a
-# ╠═c2d1d0e0-7bac-4287-8487-022691dd8c42
-# ╠═e773e9b8-2e0c-4e99-9c5a-494a5cfb1c94
-# ╠═1152ff30-eb29-4550-8503-44df407838eb
-# ╠═cb45d234-0e1d-4ce6-9dce-86320d269be2
-# ╠═5dd7f92f-bcf1-4373-91f9-de25487c0e95
-# ╠═e2520f9b-66e0-488f-8a47-ddefdd161d62
-# ╠═7087e2d5-e66e-4d19-9478-6bf13a8cfed6
